f94b988e028f72a60bbfcc5239712adc6b6e4c9c
Yeampier2022 <yeampierjesusa@gmail.com>
2023-10-18 10:45:07 -0400

    feat: rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoListComponent } from './producto-list/producto-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: 'productos', component: ProductoListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
